f772e1b850bc23582a8b8a1e54ed1c2afa9ebcd9
Mohammad Wildan Nuryulda <91609578+mwn-san@users.noreply.github.com>
2025-04-30 17:17:35 +0700

    create_topics

--- command-line-interface/tools/create_topics.sh
+++ command-line-interface/tools/create_topics.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-for i in <topic-name>
+
+# Filename containing the list of topics
+TOPICS_FILE="topics-to-create.txt"
+
+# Kafka bootstrap server
+BOOTSTRAP_SERVER="<host:port>,<host:port>,<host:port>"
+
+# Docker container name
+DOCKER_CONTAINER="BK781"
+
+# Checking if the topic list file exists
+if [ ! -f "$TOPICS_FILE" ]; then
+  echo "File $TOPICS_FILE Not found!"
+  exit 1
+fi
+
+# Reading each line in the file and deleting the topic
+while IFS= read -r TOPIC
 do
-    echo $i
-    docker exec -it <broker> kafka-topics --bootstrap-server <host:port> --create --topic $i --partitions 10 --replication-factor 5
-done
+  if [ -n "$TOPIC" ]; then
+    echo "Creating topic: $TOPIC"
+    docker exec -t "$DOCKER_CONTAINER" kafka-topics --create --topic "$TOPIC" --partitions 10 --replication-factor 5 --bootstrap-server "$BOOTSTRAP_SERVER"
+  fi
+done < "$TOPICS_FILE"
+
+echo "Topic creation process completed."
