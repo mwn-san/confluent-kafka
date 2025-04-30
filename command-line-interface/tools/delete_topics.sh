72e83c33a0025d97f0bb3e3e4a5b7fb253530c13
Mohammad Wildan Nuryulda <91609578+mwn-san@users.noreply.github.com>
2025-04-30 17:21:37 +0700

    delete_topics

--- command-line-interface/tools/delete_topics.sh
+++ command-line-interface/tools/delete_topics.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-for i in <topic-name> 
+
+# Filename containing the list of topics
+TOPICS_FILE="topics-to-delete-control-center.txt"
+
+# Kafka bootstrap server
+BOOTSTRAP_SERVER="<host:port>,<host:port>,<host:port>"
+
+# Nama container Docker
+DOCKER_CONTAINER="BK781"
+
+# Checking if the topic list file exists
+if [ ! -f "$TOPICS_FILE" ]; then
+  echo "File $TOPICS_FILE not found!"
+  exit 1
+fi
+
+# Reading each line in the file and deleting the topic
+while IFS= read -r TOPIC
 do
-    echo $i
-    docker exec -it <broker> kafka-topics --bootstrap-server <host:port> --delete --topic $i
-done
+  if [ -n "$TOPIC" ]; then
+    echo "Deleting topic: $TOPIC"
+    docker exec -t "$DOCKER_CONTAINER" kafka-topics --delete --topic "$TOPIC" --bootstrap-server "$BOOTSTRAP_SERVER"
+  fi
+done < "$TOPICS_FILE"
+
+echo "Topic deletion process completed."
